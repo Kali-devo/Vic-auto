17b03dfb5fe4e923f4a4a2271d31ba9e6f6fbd36
Kali-devo <arronsd24@gmail.com>
2026-05-24 14:32:02 +1000

    Create dashboardConfig.js

---
+++ src/config/dashboardConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    color: '#2B65D9',
+
+    banner: 'https://cdn.discordapp.com/attachments/1398475556009545881/1507957037232029826/Dashboard.png?ex=6a13c9ee&is=6a12786e&hm=4416020980b7d2dfb34256c06ee76612053f8f709d5d8f832ce8625e83939ec5&',
+
+    footer: 'Victoria State Roleplay • Australia.'
+
+};
